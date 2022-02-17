48a7e6ae4f938cbdbe5e18e17bb81b8a2e7a7cf3
danielgn95 <97767608+danielgn95@users.noreply.github.com>
2022-02-17 14:28:41 -0500

    Add files via upload

---
+++ HelloWorldDocker/src/com/danieltesting/testingdocker/Main.java
@@ -0,0 +1,18 @@
+package com.danieltesting.testingdocker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int count = 0;
+        try {
+            while (true){
+                Thread.sleep(2*1000);
+                System.out.println("I'm still here! Iteration "+count++);
+            }
+        }
+        catch (InterruptedException stopPlease){
+            stopPlease.printStackTrace();
+        }
+    }
+}
